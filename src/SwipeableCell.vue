e1da5f0d2994215a0061dc28a0e5fbe29c2f4351
RecherJ <ygliusz@foxmail.com>
2018-09-28 22:59:01 +0800

    complete swipe logic

--- src/SwipeableCell.vue
+++ src/SwipeableCell.vue
@@ -31,92 +31,68 @@
                 // 记录初始位置
                 this.startX = e.touches[0].clientX;
                 const currentTarget = e.currentTarget;
-                
+                if (currentTarget.dataset.type != 1) {
+                    this.resetSlide();
+                }
             },
             touchmove(e) {
-                console.log('touch move');
+                // console.log('touch move');
                 const startX = e.touches[0].clientX;
                 const currentTarget = e.currentTarget;
                 let deleteNode = currentTarget.childNodes[1];
                 if (currentTarget.dataset.type == 1) { // 展开状态
-                    console.log('zhankai');
                     this.delta = startX - this.startX - 80;
                 } else {
                     this.delta = startX - this.startX;
                 }
-                console.log('diff ---', this.delta);
+                // console.log('diff ---', this.delta);
                 currentTarget.style.transform = `translate3d(${this.delta}px, 0, 0)`;
                 deleteNode.style.right = `${this.delta}px`;
                 deleteNode.style.width = `${-this.delta}px`;
             },
             touchEnd(e) {
-                console.log('touch end');
+                // console.log('touch end');
                 // 当前滑动的父元素
                 let currentElement = e.currentTarget;
                 let deleteNode = currentElement.childNodes[1];
                 // 记录结束位置
                 this.endX = e.changedTouches[0].clientX;
                 const offset = this.endX - this.startX;
-                
+                currentElement.childNodes[0].style.transition = `0.3s`;
                 if (offset < -80) {
                     currentElement.style.transform = `translate3d(-80px, 0, 0)`;
                     deleteNode.style.right = `-80px`;
                     deleteNode.style.width = `80px`;
-                    this.resetSlide();
                     currentElement.dataset.type = '1';
                     this.delta = -80;
                 } else {
                     currentElement.style.transform = `translate3d(0, 0, 0)`;
                     deleteNode.style.right = `0`;
                     deleteNode.style.width = `0`;
-                    this.resetSlide();
                     currentElement.dataset.type = '0';
                     this.delta = 0;
                     this.startX = 0;
                 }
-                // 记录展开的cell
-                this.currentSpreadNode = currentElement;
-                // // 判断是左滑 还是 右滑
-                // if (currentElement.dataset.type == 0 && (this.startX - this.endX) > 50) {
-                //     this.resetSlide();
-                //     currentElement.dataset.type = "1";
-                //     console.log('--左滑--');
-                // }
-                // if (currentElement.dataset.type == 1 && (this.startX - this.endX) < -50) {
-                //     this.resetSlide();
-                //     currentElement.dataset.type = "0";
-                //     console.log('--右滑--');
-                // }
-                
+                setTimeout(() => {
+                    currentElement.childNodes[0].style.transition = ``;
+                }, 300);
                 this.endX = 0;
             },
             tap() {
-                if (this.chechSlide()) {
-                    this.resetSlide();
-                }
-            },
-            scrollTo(node, offsetX, duration = 200) {
-                node.style.transition = `${duration}ms`;
-                this.delta = 0;
-                setTimeout(() => {
-                    node.style.transition = '';
-                }, duration);
+                this.$emit('swipeable-cell-delete', this.index);
             },
             resetSlide() {
                 const listItems = document.querySelectorAll('.list-item');
                 for (let i = 0; i < listItems.length; i++) {
-                    listItems[i].dataset.type = '0';
-                }
-            },
-            // 检查当前是否有item处于滑动状态
-            chechSlide() {
-                const listItems = document.querySelectorAll('.list-item');
-                for (let i = 0; i < listItems.length; i++) {
                     if (listItems[i].dataset.type == 1) {
-                        return true;
+                        listItems[i].style.transition = `0.3s`;
+                        listItems[i].style.transform = `translate3d(0, 0, 0)`;
+                        listItems[i].dataset.type = '0'
+                        setTimeout(() => {
+                            listItems[i].style.transition = ``;
+                        }, 300);
                     }
                 }
-                return false;
             },
             deleteCell(e) {
                 const index = e.currentTarget.dataset.index;
